804c6e08896a922ca476aa2236c466b57ceecf47
GyeCheol <godmarketing1004@gamil.com>
2020-05-27 20:36:34 +0900

    ESLint, Prettier 적용

--- vue-til/src/App.vue
+++ vue-til/src/App.vue
@@ -6,19 +6,22 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "App",
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  created() {
+    console.log('hi');
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
